04b483910cb4487161a2fc4656e2078448f64773
Diego Quan <diegoquan101@gmail.com>
2021-07-08 10:38:56 -0600

    templating

--- src/components/base/template.js
+++ src/components/base/template.js
@@ -1,16 +1,52 @@
-const template_variables = {
+var template_variables = {
     "doctorName": {
-        "value": "Alejandro Madraz",
+        "value": "Alejandro Madrazo",
         "type": "innerHTML"
     },
     "doctorSlogan": {
         "value": "Medicos de verdad",
         "type": "innerHTML"
     },
-    "doctorPhoneAction": {
+    "doctorPhone": {
         "value": "+502 3386-6530",
         "type": "innerHTML"
     },
+    "doctorPhoneText" : {
+        "value": "Llama y reserva tu consulta",
+        "type": "innerHTML"
+    },
+    "doctorAddress" : {
+        "value": "0 calle, zona 9",
+        "type": "innerHTML"
+    },
+    "ServicesTitle" : {
+        "value" : "Servicios que se ofrecen",
+        "type" : "innerHTML"
+    },
+    "Service1" : {
+        "value" : "Pediatría Eficiente",
+        "type" : "innerHTML"
+    },
+    "Service2" : {
+        "value" : "Enfermeras con experiencia",
+        "type" : "innerHTML"
+    },
+    "Service3" : {
+        "value" : "Servicios de Atención Urgente",
+        "type" : "innerHTML"
+    },
+    "Service1-Text" : {
+        "value" : "Descripción 1",
+        "type" : "innerHTML"
+    },
+    "Service2-Text" : {
+        "value" : "Descripción 2",
+        "type" : "innerHTML"
+    },
+    "Service3-Text" : {
+        "value" : "Descripción 3",
+        "type" : "innerHTML"
+        }
 }
 
 
